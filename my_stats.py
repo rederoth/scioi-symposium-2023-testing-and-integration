711f1e8bafbddbd64dc907a178eb5c2effa1d04f
rederoth <roth@tu-berlin.de>
2023-10-10 12:31:10 +0200

    better median implementation and basic test

--- my_stats.py
+++ my_stats.py
@@ -1,5 +1,10 @@
 def my_median(my_list):
-    raise NotImplementedError
+    my_list.sort()
+    if len(my_list) % 2 == 0:
+        return (my_list[int(len(my_list) / 2)] + my_list[int(len(my_list) / 2) - 1]) / 2
+    else:
+        return my_list[int(len(my_list) / 2)]
+    
 
 
 def my_mean(my_list):
--- test_my_stats.py
+++ test_my_stats.py
@@ -1,2 +1,17 @@
+from my_stats import my_median
+
 def test_my_median():
-    pass
+    data = [1, 2, 3, 4, 5]
+    assert my_median(data) == 3
+
+    data = [1, 2, 3, 4, 5, 6]
+    assert my_median(data) == 3.5
+
+    data = [1, 6, 17]
+    assert my_median(data) == 6
+
+def main():
+    test_my_median()
+
+if __name__ == '__main__':
+    main()
