a036f144351e74e04f69fbb9c1edb41add04d3df
rederoth <roth@tu-berlin.de>
2023-10-10 14:36:16 +0200

    add automated test

--- my_stats.py
+++ my_stats.py
@@ -1,11 +1,16 @@
+import pytest
+
 def my_median(my_list):
     my_list.sort()
-    if len(my_list) % 2 == 0:
+    # assert len(my_list) > 0, "empty data passed to my_median"
+    if len(my_list) == 0:
+        # pass
+        raise ValueError("empty data passed to my_median")
+    elif len(my_list) % 2 == 0:
         return (my_list[int(len(my_list) / 2)] + my_list[int(len(my_list) / 2) - 1]) / 2
     else:
         return my_list[int(len(my_list) / 2)]
     
-
 
 def my_mean(my_list):
     raise NotImplementedError
--- test_my_stats.py
+++ test_my_stats.py
@@ -1,3 +1,4 @@
+import pytest
 from my_stats import my_median
 
 def test_my_median():
@@ -9,6 +10,22 @@ def test_my_median():
 
     data = [1, 6, 17]
     assert my_median(data) == 6
+
+
+def test_my_median_empty():
+    data = []
+    with pytest.raises(ValueError, match=r".*empty.*"):
+        my_median(data)
+
+@pytest.mark.parametrize("my_list, expected", [
+    ([1, 2, 3, 4, 5], 3),
+    ([1, 2, 3, 4, 5, 6], 3.5),
+    ([1, 6, 17], 6),
+    ([1, 6, 17, 2, 3], 3)
+])
+def test_my_median_par(my_list, expected):
+    assert my_median(my_list) == expected
+
 
 def main():
     test_my_median()
